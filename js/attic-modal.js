5b40434aeeac64550f01d5928559e0e58f7d5f66
Generosa Litton <generosa.litton@yahoo.com>
2017-10-22 17:17:03 -0700

    final versions

--- js/attic-modal.js
+++ js/attic-modal.js
@@ -1,9 +1,9 @@
 // Attic model js, triggered mith mouseover 
 var utils = window.optimizely.get("utils");
-       
+
 function hideAtticModal() {
   document.querySelector('.attic-modal').style.display = "none";
-  // This sets the Optimizely log
+  // This creates a log about when users close the modal
   window['optimizely'] = window['optimizely'] || [];
   window['optimizely'].push({
     type: "event",
@@ -12,36 +12,28 @@ function hideAtticModal() {
 }
 // Function that triggers the Attic and Button modal
 function showAtticModal() {
-    document.querySelector('.attic-modal').style.display = "block";
+   document.querySelector('.attic-modal').style.display = "block";
+}
+
+// Captures the Add To Cart click event
+function modalAddToCart(evt) {
+  evt.preventDefault();
+  //Access the item in the array
+  $('#AddToCart')[0].click();
 }
 
 //Loads page first before executing events
 utils.waitForElement('body').then(function(element) {
   var html = widget.$html;
   element.insertAdjacentHTML('beforeend', html);
+  
   // Hover over Add To Cart button and show modal only once
   $('button#AddToCart.btn').one('mouseover', showAtticModal);
   document.querySelector('span.attic-modal-close').addEventListener('click',
     hideAtticModal);
+  
+  document.getElementById('modal-add-to-cart').addEventListener('click', modalAddToCart); 
 });
 
-// Function that adds item to cart from the attic modal button
-$(function() {
-  $('#attic-cart-submit').on('click', function(evt) {
-    evt.preventDefault();
-    $.ajax({
-        type: "POST",
-        url: "/cart/add",
-        data: $('form.tagForm').serialize(),// what do i put in here??
-        success: function(response) {
-            alert(response['response']);
-        },
-        error: function() {
-            alert('Error');
-        }
-    });
-    return false;
-  });
-});
 
 
